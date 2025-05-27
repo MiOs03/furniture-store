363b2f2291fc44d5179af7d8bb90cdbdc46cc2f2
Your Name <your.email@example.com>
2025-05-27 12:28:32 +0200

    chore: update Resend API key

--- lib/config.ts
+++ lib/config.ts
@@ -1,6 +1,6 @@
 export const config = {
   resend: {
-    apiKey: process.env.RESEND_API_KEY || 're_ca1tVNQQ_3sqw8dKgyBDgjBA6YWBmJ8wT',
+    apiKey: process.env.RESEND_API_KEY || 're_WHfmTGCs_23hehkrVa76bYQtkkfyponM8',
     fromEmail: process.env.RESEND_FROM_EMAIL || 'onboarding@resend.dev',
     companyEmail: process.env.COMPANY_EMAIL || 'lukic.vlad@gmail.com'
   }
